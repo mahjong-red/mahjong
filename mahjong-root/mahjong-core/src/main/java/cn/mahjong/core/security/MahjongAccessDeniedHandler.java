4ce7eba53bef887b83827eda86631668befe1929
Administrator <Administrator@jmpc>
2017-06-19 23:13:48 +0800

    error

--- mahjong-root/mahjong-core/src/main/java/cn/mahjong/core/security/MahjongAccessDeniedHandler.java
+++ mahjong-root/mahjong-core/src/main/java/cn/mahjong/core/security/MahjongAccessDeniedHandler.java
@@ -8,7 +8,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.SpringSecurityMessageSource;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
@@ -24,6 +26,8 @@ public class MahjongAccessDeniedHandler implements AccessDeniedHandler {
 	
 	private static final String RESPONSE_TYPE_JSON = "application/json";
 	
+	public MessageSourceAccessor messages = SpringSecurityMessageSource.getAccessor();
+	
 	@Override
 	public void handle(HttpServletRequest request,
 			HttpServletResponse response,
@@ -33,7 +37,7 @@ public class MahjongAccessDeniedHandler implements AccessDeniedHandler {
 		String header = request.getHeader("accept");
 		response.setStatus(HttpStatus.SC_FORBIDDEN);
 		if (StringUtils.contains(header, RESPONSE_TYPE_JSON)){
-			RestResp resp = new RestResp("999", "Access Denied", null);
+			RestResp resp = new RestResp("999", messages.getMessage("AbstractAccessDecisionManager.accessDenied", "权限不足，拒绝访问。") , null);
 			response.getWriter().print(JSONObject.toJSONString(resp));
 			return;
 		}
