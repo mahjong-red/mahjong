18d73d1ee1cc0389feb99dc09a103d280c15b67d
Administrator <Administrator@jmpc>
2017-06-22 23:26:13 +0800

    非法所得所放大

--- mahjong-root/mahjong-api/src/main/java/cn/mahjong/rest/controller/TestController.java
+++ mahjong-root/mahjong-api/src/main/java/cn/mahjong/rest/controller/TestController.java
@@ -1,6 +1,7 @@
 package cn.mahjong.rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,13 +18,16 @@ public class TestController {
 	@Autowired
 	public UserService userService;
 	
+	@Autowired
+	private RedisTemplate redisTemplate;
 	
 	@RequestMapping("getbyid")
 	@ResponseBody
 	public RestResp get(int id){
 		RestResp resp = new RestResp("0", "", null);
-		User news = (UserImpl) userService.getObject(UserImpl.class, 1);
-		resp.setData(news);
+		User user = (UserImpl) userService.getObject(UserImpl.class, 1);
+		redisTemplate.opsForValue().set("user","hello world");
+		resp.setData(user);
 		return resp;
 	}
 }
---
+++ mahjong-root/mahjong-core/src/main/java/cn/mahjong/core/redis/MahjongKeyGenerator.java
@@ -0,0 +1,22 @@
+package cn.mahjong.core.redis;
+
+import java.lang.reflect.Method;
+
+import org.springframework.cache.interceptor.KeyGenerator;
+
+public class MahjongKeyGenerator implements KeyGenerator{
+
+	@Override
+	public Object generate(Object target, Method method, Object... params) {
+		StringBuilder sb = new StringBuilder();  
+        sb.append(target.getClass().getName());
+        sb.append(":");
+        sb.append(method.getName());  
+        sb.append(":");
+        for (Object obj : params) {  
+            sb.append(obj.toString());  
+        }  
+        return sb.toString();
+	}
+
+}
