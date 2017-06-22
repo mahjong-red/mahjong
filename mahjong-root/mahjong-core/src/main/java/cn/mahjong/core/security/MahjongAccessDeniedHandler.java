3f525dd949c010acfaa2012b3a949702ddbeb4f6
Administrator <Administrator@jmpc>
2017-06-22 23:24:55 +0800

    error msg

--- mahjong-root/mahjong-core/src/main/java/cn/mahjong/core/security/MahjongAccessDeniedHandler.java
+++ mahjong-root/mahjong-core/src/main/java/cn/mahjong/core/security/MahjongAccessDeniedHandler.java
@@ -8,15 +8,16 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.SpringSecurityMessageSource;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
-import cn.mahjong.dto.RestResp;
-
 import com.alibaba.fastjson.JSONObject;
+
+import cn.mahjong.dto.RestResp;
 
 /**
  * 访问被拒绝
@@ -26,7 +27,8 @@ public class MahjongAccessDeniedHandler implements AccessDeniedHandler {
 	
 	private static final String RESPONSE_TYPE_JSON = "application/json";
 	
-	public MessageSourceAccessor messages = SpringSecurityMessageSource.getAccessor();
+	@Autowired
+	public ResourceBundleMessageSource messageSource;
 	
 	@Override
 	public void handle(HttpServletRequest request,
@@ -37,7 +39,7 @@ public class MahjongAccessDeniedHandler implements AccessDeniedHandler {
 		String header = request.getHeader("accept");
 		response.setStatus(HttpStatus.SC_FORBIDDEN);
 		if (StringUtils.contains(header, RESPONSE_TYPE_JSON)){
-			RestResp resp = new RestResp("999", messages.getMessage("AbstractAccessDecisionManager.accessDenied", "权限不足，拒绝访问。") , null);
+			RestResp resp = new RestResp("403", messageSource.getMessage("AbstractAccessDecisionManager.accessDenied", null, "", LocaleContextHolder.getLocale()) , null);
 			response.getWriter().print(JSONObject.toJSONString(resp));
 			return;
 		}
